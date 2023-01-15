63e610f610a7c111fdd164e95e6e0033a79c60de
banma <banma@5kcrm.com>
2023-01-15 16:00:34 +0800

    删除旧版本前端文件

--- module/src/main/webapp/public/static/url.js
+++
@@ -1,9 +0,0 @@
-const urlSearch = window.location.search
-if (urlSearch && location.hash) {
-  const href = window.location.href
-  const hashIndex = href.indexOf(location.hash)
-  const searchIndex = href.indexOf(location.search)
-  if (hashIndex > searchIndex) {
-    window.location.href = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + location.pathname + location.hash + urlSearch
-  }
-}
